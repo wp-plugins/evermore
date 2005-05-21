b465c38fc78926f337e3ecf9214a97fe1b559f50
bennettmcelwee <bennettmcelwee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-05-21 12:55:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/evermore/trunk@1838 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,37 @@
+=== Evermore ===
+
+Tags: extract,more,abbreviate,short
+Contributors: bennettmcelwee
+
+Evermore automatically abbreviates all posts when they appear on a multiple-post page such as the main blog page. It has the same effect as putting  <!--more--> after the first paragraph of every post. All formatting and HTML tags are preserved in the abbreviated post.
+
+If the post already has a <!--more--> in it, then this plugin does nothing to it and the existing <!--more--> will behave as usual.
+
+If you want to disable the plugin for any specific post, then include the codeword <!--nevermore--> in the post. This won't show up in the post, but it will prevent the post from being abbreviated by Evermore. 
+
+
+== Installation ==
+
+1. Copy evermore.php into your WordPress plugins directory (wp-content/plugins).
+2. Log in to WordPress Admin. Go to the Plugins page and click Activate for Evermore
+
+
+== Frequently Asked Questions ==
+
+
+= What is this good for? =
+
+The main page of a blog can often be too full. By default, WordPress displays the last ten posts on the main page, so viewers have to do a lot of scrolling to see if there's something interesting. With Evermore, all posts of the main page are abbreviated down to the first paragraph, so visitors can easily scan  your posts, find something interesting, and become hooked.
+
+= I want one of my posts to appear in full! =
+
+Include the magic word <!--nevermore--> in any post and Evermore will ignore that post. The magic word won'
+t appear when people view your post, but Evermore will see it and obey.
+
+= I've changed my mind. How can I get my posts back the way they were? =
+
+Just go to your WordPress Admin screen and disable the Evermore plugin. Everything will be back as it was.
+
+= Why not just add <!--more--> to my posts? =
+
+You can manually add <!--more--> to all your posts. This has exactly the same effect as Evermore. But with Evermore, you can turn the abbreviations on or off with a few clicks just by enabling or disabling the plugin. With the manual way, you have to manually go in and edit each post.
--- evermore.php
+++ evermore.php
@@ -1,23 +1,40 @@
 <?php
 /*
 Plugin Name: Evermore
-Plugin URI: http://www.thunderguy.com/semicolon/
-Description: Make all posts behave as if there is a "&lt;!--more--&gt;" after the first paragraph.
-Version: 0.1
+Plugin URI: http://www.thunderguy.com/semicolon/evermore-wordpress-plugin/
+Description: Abbreviate all posts when viewed on multiple post pages. This makes all posts behave as if there is a "&lt;!--more--&gt;" after the first paragraph.
+Version: 1.0
 Author: Bennett McElwee
 Author URI: http://www.thunderguy.com/semicolon/
 */ 
 
-/*	==================================================
-	
+/*
+INSTALLATION
+
+1. Copy this file into the plugins directory in your WordPress installation (wp-content/plugins).
+
+2. Log in to WordPress Admin. Go to the Plugins page and click Activate for this plugin.
+
+USAGE
+
+Evermore automatically abbreviates all posts when they appear on a multiple-post page such as the main blog page. It has the same effect as putting  <!--more--> after the first paragraph of every post. All formatting and HTML tags are preserved in the abbreviated post.
+
+If the post already has a <!--more--> in it, then this plugin does nothing to it and the existing <!--more--> will behave as usual.
+
+If you want to disable the plugin for any specific post, then include the codeword <!--nevermore--> in the post. This won't show up in the post, but it will prevent the post from being abbreviated by Evermore. 
+
+DEVELOPMENT NOTES
+
+All globals begin with "tguy_em_" (for Thunderguy Evermore)
+Tested with PHP 4.3.8, WordPress 1.5.
 */
 
-// Add the "more" link immediately after reading from the database
+// Add the "more" link immediately after reading the post from the database
 add_filter('the_posts', 'tguy_em_addmoreall');
 
+function tguy_em_addmoreall($posts) {
 /*	Add a "more" link immediately after reading posts from the database.
 */
-function tguy_em_addmoreall($posts) {
 	$count = count($posts);
 	for ($i = 0; $i < $count; ++$i) {
 		$posts[$i]->post_content = tguy_em_addmore($posts[$i]->post_content);
@@ -25,27 +42,23 @@ function tguy_em_addmoreall($posts) {
 	return $posts;
 }
 
+function tguy_em_addmore($post_content) {
 /*	Add a "more" comment between the first and second paragraphs, unless
 	there is already a "<!--more-->" (we don't add an extra one) or
 	a "<!--nevermore-->" (user has disabled evermore for this post).
 */
-function tguy_em_addmore($post_content) {
 	// Only continue if content has no "more" and no "nevermore"
 	if ((false === strpos($post_content, '<!--more-->'))
 	&&  (false === strpos($post_content, '<!--nevermore-->'))) {
-		// Get the first paragraph including all surrounding whitespace
-		 if (preg_match('!^(\s*.*?(?:</(?:p|pre|blockquote|div)>|(?:\r\n|\r|\n){2})\s*)\S!', $post_content, $matches)) {
+		// Get the first paragraph including all surrounding whitespace.
+		// First paragraph is the first closing block tag or the first
+		// double newline, whichever comes first.
+		 if (preg_match('!^(\s*.*?(?:</(?:p|pre|blockquote|div|ol|ul)>|(?:\r\n|\r|\n){2})\s*)\S!', $post_content, $matches)) {
 			$firstPara = $matches[1];
 			return $firstPara . '<!--more-->' . substr($post_content, strlen($firstPara));
 		}
 	}
 	return $post_content;
 }
-
-
-/*	==================================================
-	Template functions
-*/
-
 
 ?>
