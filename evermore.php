c282b9bf291ac7fcf58070794bfad8ad982c83ac
bennettmcelwee <bennettmcelwee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-05-25 11:45:53 +0000

    Add GPL text
    git-svn-id: https://plugins.svn.wordpress.org/evermore/trunk@1916 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- evermore.php
+++ evermore.php
@@ -6,6 +6,28 @@ Description: Abbreviate all posts when viewed on multiple post pages. This makes
 Version: 1.0
 Author: Bennett McElwee
 Author URI: http://www.thunderguy.com/semicolon/
+
+Copyright (C) 2005 Bennett McElwee
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the
+Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+The GNU General Public License is also available at
+http://www.gnu.org/copyleft/gpl.html
+
+Bennett McElwee, bennett at thunderguy dotcom
 */ 
 
 /*
